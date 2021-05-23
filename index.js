538e7b5d889a4a9827ebe72b8627a5ea7715b6ff
bianca.lazzaris <bianca.lazzaris@gmail.com>
2021-05-23 19:33:15 -0300

    estrutura

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
