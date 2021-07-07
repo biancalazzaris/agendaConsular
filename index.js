160cb8f32b4ddca63cad12ba1e46ef0ea59a0a71
bianca.lazzaris <bianca.lazzaris@gmail.com>
2021-07-06 21:31:01 -0300

    const bodyParser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
